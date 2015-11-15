aeaafe6c5d91dc3bf1c252ddef9687d00385c83d
Andriy Homyak <AndrewHamster@hotmail.com>
2015-11-15 18:53:29 +0200

    solved 1.8/10

--- stepic 1.8/10.cpp
+++ stepic 1.8/10.cpp
@@ -4,26 +4,34 @@ using namespace std;
 
 int main() {
 	int n , m;
+
 	cin >> n >> m;
-	int a[i], c = 1, dir = 1;
+
+	int a[100][100];
+
+	int c = 1, dir = 1;
+
+
 	for (int i = 0; i < n; i++){
-		for (int j = 0; j < m; j += dir){
+		for (int j = 0; j < m; j ++){
 			if(dir == 1){
-				a[i][j] = c++;
+				a[i][j] = c;
 			}	else	{
-				a[i][m-j] = c++;
+				a[i][m-j-1] = c;
 			}
+			c++;
 		}
 		dir *= -1;
 	}
-	
+
 	for (int i = 0; i < n; i++){
 		for (int j = 0; j < m; j++){
 			cout.width(4);
-			cout << a[i][j]; 
+			cout << a[i][j];
 		}
 		cout << endl;
 	}
-	
+
 	return 0;
 }
+
